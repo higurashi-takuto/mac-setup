95563ea681e3923ff8a97de3ab3f6292c6e13b12
higurashi-takuto <takuto.8726.higurashi@gmail.com>
2019-10-09 20:49:12 +0900

    Homebrewを追記

--- setup.sh
+++ setup.sh
@@ -4,16 +4,11 @@
 defaults write com.apple.finder AppleShowAllFiles TRUE
 killall Finder
 
-# subl
-sudo ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
-
 # Homebrew
+wget https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.Brewfile
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# zsh
-brew install zsh
-echo /usr/local/bin/zsh | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/zsh
+brew tap homebrew/bundle
+brew bundle --global
 
 # prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -21,27 +16,3 @@ setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-
-# env
-brew install nodenv pyenv pyenv-virtualenv
-
-# Python
-sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
-env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3\.?*' | tail -1)
-env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3.6\.?*' | tail -1)
-pyenv virtualenv 3.6.6 global
-pyenv global global
-pip install --upgrade pip
-pip install flake8
-
-# Heroku
-brew install heroku/brew/heroku
-heroku login
-
-# TeX
-brew cask install basictex
-brew install ghostscript
-sudo tlmgr update --self --all
-sudo tlmgr install collection-langjapanese
-sudo mktexlsr
-sudo kanji-config-updmap-sys tp
