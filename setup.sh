f7bb305f2447ae725664a4112f8a9297ce97ea10
higurashi-takuto <takuto.8726.higurashi@gmail.com>
2019-10-11 10:37:37 +0900

    envの追加

--- setup.sh
+++ setup.sh
@@ -20,3 +20,16 @@ done
 # git
 curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+
+# .zsh
+echo /usr/local/bin/zsh | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/zsh
+curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.zshrc
+source .zshrc
+
+# python
+env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3\.?*' | tail -1)
+pyenv virtualenv $(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3\.?*' | tail -1) global
+pyenv global global
+pip install --upgrade pip
+pip install flake8
