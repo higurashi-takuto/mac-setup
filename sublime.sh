49a24686e2aa23b3bff19e42d9579a79af88f805
higurashi-takuto <contact@hgrs.me>
2020-02-24 02:16:45 +0900

    Sublime Merge の設定を追記
    キーバインドと起動コマンド

--- sublime.sh
+++ sublime.sh
@@ -1,5 +1,7 @@
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+ln -s /Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge /usr/local/bin/smerge
 git clone https://github.com/higurashi-takuto/sublime-dark-theme.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/sublime-dark-theme
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/Package\ Control.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/Preferences.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/SublimeLinter.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/Default.sublime-keymap > ~/Library/Application\ Support/Sublime\ Merge/Packages/UserDefault.sublime-keymap
