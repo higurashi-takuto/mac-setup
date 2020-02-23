834b0ed92c2d0ca3362265e31c642bee66e7a683
higurashi-takuto <contact@hgrs.me>
2020-02-23 16:13:24 +0900

    sublime と Git 周りを整備

---
+++ sublime.sh
@@ -0,0 +1,5 @@
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+git clone https://github.com/higurashi-takuto/sublime-dark-theme.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/sublime-dark-theme
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/Package\ Control.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/Preferences.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/SublimeLinter.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
