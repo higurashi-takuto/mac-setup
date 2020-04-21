07ddb66e0dcb19097d04f90c212f7cf0b0000faf
higurashi-takuto <contact@hgrs.me>
2020-04-22 00:41:19 +0900

    ファイル整理

--- README.md
+++ README.md
@@ -5,6 +5,35 @@ $ xcode-select --install
 $ curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/setup.sh | zsh
 ```
 
+## sublime.sh
+Sublime Text / Sublime Merge の設定
+```console
+$ curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/sublime.sh | zsh
+```
+
+## resources/
+### .Brewfile
+Homebrew は便利。
+
+### .gitignore_global
+macOS 用にいろいろ無視。
+
+### .zshrc
+Sublime Text 以外は許さん。
+
+### iTerm2.json
+iTerm2 用のプロファイル。
+
+### jupyterlab.plist
+JupyterLab を自動起動にする設定ファイル。
+
+## sublime/
+Sublime Text / Sublime Merge の設定ファイル。
+
+## wallpapers/
+壁紙の画像。
+
+## その他設定
 JupyterLab で外部アクセスを許可する場合は以下のコマンドも実行する。
 ```console
 $ jupyter notebook password
@@ -16,33 +45,6 @@ MacBook 系を使っている場合は充電音を切るため、以下のコマ
 $ defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
 $ killall PowerChime
 ```
-
-## sublime.sh
-Sublime Text の設定
-```console
-$ curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/sublime.sh | zsh
-```
-
-## .Brewfile
-Homebrew は便利。
-
-## .Brewfile_lite
-使用頻度の低いものを削除。
-```console
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.Brewfile_lite > .Brewfile
-```
-
-## .gitignore_global
-macOS 用にいろいろ無視。
-
-## .zshrc
-Sublime Text 以外は許さん。
-
-## iTerm2.json
-iTerm2 用のプロファイル。
-
-## jupyterlab.plist
-JupyterLab を自動起動にする設定ファイル。
 
 ## 個別でインストールするソフト一覧
 - [UAD SOFTWARE](https://www.uaudio.jp/uad/downloads/)
--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ killall Finder
 
 # Homebrew
 if [ ! -e .Brewfile ]; then
-  curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.Brewfile
+  curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.Brewfile
 fi
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/bundle
@@ -21,18 +21,18 @@ done
 sed -i '' s/'sorin'/'powerline'/ .zprezto/runcoms/zpreztorc
 
 # git
-curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.gitignore_global
+curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 # .zsh
 echo /usr/local/bin/zsh | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/.zshrc >| .zprezto/runcoms/zshrc
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.zshrc >| .zprezto/runcoms/zshrc
 source .zshrc
 
 # python
 \mkdir Library/LaunchAgents
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/jupyterlab.plist > Library/LaunchAgents/jupyterlab.plist
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/jupyterlab.plist > Library/LaunchAgents/jupyterlab.plist
 NEWEST_PYTHON_VERSION=$(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3\.?*' | tail -1 | tr -d ' ')
 env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $NEWEST_PYTHON_VERSION
 pyenv virtualenv $NEWEST_PYTHON_VERSION global
@@ -60,6 +60,6 @@ rbenv install $NEWEST_RUBY_VERSION
 rbenv global $NEWEST_RUBY_VERSION
 
 # wallpaper
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/wallpaper/KaguyaLuna.png > Pictures/KaguyaLuna.png
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/wallpaper/HatsuneMiku.png > Pictures/HatsuneMiku.png
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/wallpapers/KaguyaLuna.png > Pictures/KaguyaLuna.png
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/wallpapers/HatsuneMiku.png > Pictures/HatsuneMiku.png
 wallpaper set ~/Pictures/KaguyaLuna.png
