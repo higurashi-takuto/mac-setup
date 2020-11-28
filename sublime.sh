92e0dcde7928ad45d49e68287d013ea8d8e4fd9e
higurashi-takuto <contact@hgrs.me>
2020-11-28 18:35:30 +0900

    Markdown の設定

--- sublime.sh
+++ sublime.sh
@@ -1,6 +1,7 @@
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 ln -s /Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge /usr/local/bin/smerge
 git clone https://github.com/higurashi-takuto/sublime-dark-theme.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/sublime-dark-theme
+curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/sublime/OmniMarkupPreviewer.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/OmniMarkupPreviewer.sublime-settings
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/sublime/Package\ Control.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/sublime/Preferences.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/sublime/SublimeLinter.sublime-settings > ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/SublimeLinter.sublime-settings
