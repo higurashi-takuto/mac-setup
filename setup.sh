dee0345347cc25ddb513eb9167a53bac9a8719f0
Higurashi Takuto <contact@hgrs.me>
2022-11-05 01:47:37 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,23 +30,8 @@ chsh -s /usr/local/bin/zsh
 curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.zshrc > .zprezto/runcoms/zshrc
 source .zshrc
 
-# python
-NEWEST_PYTHON_VERSION=$(pyenv install -l | grep -v '[a-zA-Z]' | grep -e '\s3\.?*' | tail -1 | tr -d ' ')
-env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $NEWEST_PYTHON_VERSION
-pyenv virtualenv $NEWEST_PYTHON_VERSION global
-pyenv global global
-pip install --upgrade pip
-pip install --upgrade setuptools
-
-# ruby
-NEWEST_RUBY_VERSION=$(rbenv install -l | grep -v '[a-zA-Z]' | tail -1)
-rbenv install $NEWEST_RUBY_VERSION
-rbenv global $NEWEST_RUBY_VERSION
-
-# node
-NEWEST_NODE16_VERSION=$(nodenv install -l | grep -v '[a-zA-Z]' | grep -e '^16\.?*' | tail -1 | tr -d ' ')
-nodenv install $NEWEST_NODE16_VERSION
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.vuerc > .vuerc
-
-# tmux
-curl -L https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.tmux.conf > .tmux.conf
+# asdf
+curl -OL https://raw.githubusercontent.com/higurashi-takuto/mac-setup/master/resources/.asdfrc
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs lts
+asdf global nodejs lts
